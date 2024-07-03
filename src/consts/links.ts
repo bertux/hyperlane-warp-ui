98423f741126b7121b880d0ee06d90d0a6b5846a
Bertrand Juglas <b@juglas.name>
2024-07-04 01:41:21 +0200

    customize for Arthera

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,11 +1,11 @@
 export const links = {
-  home: 'https://www.hyperlane.xyz',
-  explorer: 'https://explorer.hyperlane.xyz',
-  discord: 'https://discord.gg/VK9ZUy3aTV',
-  github: 'https://github.com/hyperlane-xyz/hyperlane-warp-ui-template',
-  docs: 'https://docs.hyperlane.xyz',
+  home: 'https://www.arthera.net',
+  explorer: 'https://explorer.arthera.net',
+  discord: 'https://discord.gg/arthera',
+  github: 'https://github.com/bertux/hyperlane-warp-ui',
+  docs: 'https://docs.arthera.net',
   gasDocs: 'https://docs.hyperlane.xyz/docs/reference/hooks/interchain-gas',
   chains: 'https://docs.hyperlane.xyz/docs/resources/domains',
-  twitter: 'https://twitter.com/hyperlane',
-  blog: 'https://medium.com/hyperlane',
+  twitter: 'https://twitter.com/artherachain',
+  blog: 'https://medium.com/arthera',
 };
