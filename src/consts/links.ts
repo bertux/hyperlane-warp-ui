af88d30bbef153994c7c65d27189170393976b71
Bertrand Juglas <b@juglas.name>
2024-09-10 23:14:21 +0200

    Add Piccadilly chain and tokens

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,11 +1,11 @@
 export const links = {
-  home: 'https://www.arthera.net',
-  explorer: 'https://explorer.arthera.net',
-  discord: 'https://discord.gg/arthera',
+  home: 'https://www.autonity.org',
+  explorer: 'https://piccadilly.autonity.org',
+  discord: 'https://discord.gg/autonity',
   github: 'https://github.com/bertux/hyperlane-warp-ui',
-  docs: 'https://docs.arthera.net',
+  docs: 'https://docs.autonity.org',
   gasDocs: 'https://docs.hyperlane.xyz/docs/reference/hooks/interchain-gas',
   chains: 'https://docs.hyperlane.xyz/docs/resources/domains',
-  twitter: 'https://twitter.com/artherachain',
-  blog: 'https://medium.com/arthera',
+  twitter: 'https://twitter.com/autonity',
+  blog: 'https://medium.com/autonity',
 };
